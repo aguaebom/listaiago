4f550cc3f20314d2fa824a4cb3a2645675e71bcf
Ana Júlia <gouveiaapenasisso@gmail.com>
2025-06-16 08:36:53 -0300

    Create Média_aritimética.c

---
+++ Média_aritimética.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+int a,b,c,d,e,f,soma,média = 0;
+scanf("%d",&a);
+scanf("%d",&b);
+scanf("%d",&c);
+scanf("%d",&d);
+scanf("%d",&e);
+scanf("%d",&f);
+soma = a+b+c+d+e+f;
+    média = soma/6;
+    
+printf("A média é:%d",média);
+    return 0;
+}
