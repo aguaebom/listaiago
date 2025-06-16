7426738348fad15f3343c0a174d20b9bfa10a614
Ana Júlia <gouveiaapenasisso@gmail.com>
2025-06-16 08:38:39 -0300

    Create Número_par.c

---
+++ Número_par.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+ int num;
+    printf("Insira um número:\n");
+scanf("%d", &num);
+if (num % 2 == 0){
+    printf("O número é par :D");
+}else{
+    printf("O número é impar :D");
+}
+    return 0;
+}
